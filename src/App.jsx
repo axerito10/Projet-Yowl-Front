f6680a257e4f3481a7f32fb9afe184dc3a754c19
axerito10 <alexandre.chan-tien@epitech.digital>
2024-01-23 13:19:25 +0100

    [Fix] Add gitignore for node_modules

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-blue p-4">
+      <h1 className="text-2xl font-bold">Mon Application React avec Tailwind CSS</h1>
+    </div>
+  );
+}
+
+export default App;
