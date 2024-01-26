de2425c45f73b97351201f64952a5d306bdbf527
axerito10 <alexandre.chan-tien@epitech.digital>
2024-01-26 11:45:52 +0100

    [Fix] Register and Login CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ export const theme = {
     colors: {
       'custom-orange': 'rgb(255, 119, 1)',
       'custom-blue': 'rgb(0, 7, 69)',
+      'custom-hoverorange': 'rgb(234, 88, 12)',
+      'custom-gray': 'rgb(242, 242, 242)',
     },
   },
 };
