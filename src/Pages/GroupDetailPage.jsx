cae67627dee0fa296843c165bfb551423293eb89
nicolassdlp <nicolas.delepaut@epitech.digital>
2024-01-29 10:25:23 +0100

    [Feat] create detail, category page and setup homepage

---
+++ src/Pages/GroupDetailPage.jsx
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { IoIosArrowBack } from "react-icons/io";
+
+const GroupDetailPage = () => {
+  const [group, setGroup] = useState(null);
+  const [showDescription, setShowDescription] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`http://localhost:1337/api/groupes/${id}?populate=*`)
+      .then((response) => response.json())
+      .then((data) => {
+        const groupData = data.data;
+        setGroup({
+          ...groupData.attributes,
+          Proprietaire: groupData.attributes.Proprietaire,
+          category: groupData.attributes.categories.data[0]?.attributes.category, // Récupération de la catégorie
+          ImageUrl: groupData.attributes.image?.data?.attributes?.url
+            ? `http://localhost:1337${groupData.attributes.image.data.attributes.url}`
+            : null,
+          Titre_contenu: groupData.attributes.Titre_contenu,
+          Description_contenu: groupData.attributes.Description_contenu,
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching group details: ', error);
+      });
+  }, [id]);
+
+  const handleCategoryClick = () => {
+    // Redirection vers la page des groupes de la même catégorie
+    navigate(`/categorie/${group.category}`);
+  };
+
+  const toggleDescription = () => { // Définition de la fonction toggleDescription
+    setShowDescription(!showDescription);
+  };
+
+  if (!group) {
+    return <div>Loading...</div>;
+  }
+
+return (
+    <>
+      <header className="bg-white flex justify-between items-center p-4">
+        {/* Logo SKILLS à gauche */}
+        <div className="flex items-center">
+          <img
+            className="h-11 align-middle ml-24" 
+            src="https://cdn.discordapp.com/attachments/1182732629573910569/1196830881592131677/Logo_2_Skills.png"
+            alt="Skills logo"
+          />
+          <span className="text-6xl font-bold text-custom-blue">KILLS</span>
+        </div>
+
+
+        {/* Avatar de profil à droite */}
+        <div className="flex items-center mr-24">
+          <img
+            src='../public/no-profil-picture.png' 
+            alt="Profile"
+            className="w-20 border-gray-300 mr-13"
+          />
+        </div>
+      </header>
+      <div className="flex items-center justify-between mx-auto px-4 py-4 mr-24">
+        <div className="flex items-center ml-24">
+          {/* Flèche de retour */}
+          <button 
+            onClick={() => navigate(-1)} 
+            className="text-white text-2xl bg-custom-blue rounded-full p-2"
+          >
+            <IoIosArrowBack />
+          </button>
+
+          {/* Bouton de la catégorie */}
+          {group.category && (
+            <button
+            onClick={handleCategoryClick}
+              className="ml-4 text-custom-orange bg-custom-blue rounded-full py-1 px-3 text-lg"
+            >
+              {group.category}
+            </button>
+          )}
+        </div>
+
+        {/* Bouton du propriétaire déplacé à droite */}
+        {group.Proprietaire && (
+          <button
+            className="text-custom-orange bg-custom-blue rounded-full py-1 px-3 text-lg"
+          >
+            {group.Proprietaire}
+          </button>
+        )}
+      </div>
+
+      {/* Image du groupe */}
+      <div className="container mx-auto px-4 my-8">
+        {group.ImageUrl && (
+          <img 
+            src={group.ImageUrl} 
+            alt={group.Titre} 
+            className="w-full h-40 object-cover rounded-lg" 
+          />
+        )}
+      </div>
+      <div className="container mx-auto px-4 my-8 text-custom-blue">
+        <h2 className="text-2xl font-bold my-4">{group.Titre}</h2>
+        <p>{group.Description}</p>
+        </div>
+        <div className="container mx-auto px-4 my-8 text-custom-blue">
+        <h2 className="text-2xl font-bold mb-4 text-custom-blue">LES COURS</h2>
+        <button
+          className="flex items-center px-4 py-2 bg-custom-blue font-bold rounded-lg shadow-lg focus:outline-none "
+          onClick={toggleDescription}
+        >
+          <span className="mr-2 text-custom-orange">{group.Titre_contenu}</span> {/* Ajout de la classe mr-2 pour la marge à droite */}
+          <span className={`text-custom-yellow transform transition-transform ${showDescription ? 'rotate-90' : 'rotate-0'}`}>
+            &#9656;
+          </span>
+        </button>
+        {showDescription && (
+          <div className="mt-4 p-4 border rounded-lg">
+            {group.Description_contenu}
+          </div>
+        )}
+      </div>
+      <div className="flex justify-center items-center mt-72 mb-8">
+        <button className="bg-custom-blue text-custom-orange py-2 px-4 rounded-lg text-lg font-bold">
+          REJOINDRE
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default GroupDetailPage;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../Pages/Style/WelcomePage.css';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:1337/api/groupes?populate=*')
+      .then((response) => response.json())
+      .then((data) => {
+        const transformedGroups = data.data.map(item => {
+          const imageUrl = item.attributes.image?.data?.attributes?.url
+            ? `http://localhost:1337${item.attributes.image.data.attributes.url}`
+            : null;
+
+          return {
+            id: item.id,
+            ...item.attributes,
+            ImageUrl: imageUrl,
+          };
+        });
+        setGroups(transformedGroups);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []); 
+
+  // Paramètres de configuration pour react-slick
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
+return (
+    <>
+      <header className="bg-white flex justify-between items-center p-4">
+        {/* Logo SKILLS à gauche */}
+        <div className="flex items-center">
+          <img
+            className="h-11 align-middle ml-24" 
+            src="https://cdn.discordapp.com/attachments/1182732629573910569/1196830881592131677/Logo_2_Skills.png"
+            alt="Skills logo"
+          />
+          <span className="text-6xl font-bold text-custom-blue">KILLS</span>
+        </div>
+
+
+        {/* Avatar de profil à droite */}
+        <div className="flex items-center mr-24">
+          <img
+            src='../public/no-profil-picture.png' 
+            alt="Profile"
+            className="w-20 border-gray-300 mr-13"
+          />
+        </div>
+      </header>
+
+      <section className="container mx-auto px-4 my-8">
+        <h2 className="text-2xl font-bold mb-4 text-custom-blue">LES PLUS POPULAIRES</h2>
+        <Slider {...settings}>
+        {groups.map((group) => (
+          <Link to={`/group/${group.id}`} key={group.id}>
+          <div key={group.id} className="group card relative p-5">
+            <img 
+              src={group.ImageUrl} 
+              alt={group.Titre} 
+              className="w-full h-40 object-cover rounded-lg transition duration-300 ease-in-out group-hover:opacity-50" 
+            />
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+              <span className="text-custom-blue text-xl font-bold">{group.Titre}</span>
+            </div>
+          </div>
+          </Link>
+        ))}
+        </Slider>
+      </section>
+      <section className="container mx-auto px-4 my-8">
+      <h2 className="text-2xl font-bold mb-4">VOS GROUPES</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {groups.map((group) => (
+          <div key={group.id} className="group card relative mx-4">
+            <img 
+              src={group.ImageUrl} 
+              alt={group.Titre} 
+              className="w-full h-40 object-cover rounded-lg transition duration-300 ease-in-out group-hover:opacity-50" 
+            />
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+              <span className="text-custom-blue text-xl font-bold">{group.Titre}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  </>
+);
+};
+
+export default HomePage;
