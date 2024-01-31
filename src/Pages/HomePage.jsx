1df08e0397ed1bcee99b4bcd625c06edbc1e2f0c
axerito10 <alexandre.chan-tien@epitech.digital>
2024-01-31 11:24:48 +0100

    [Fix] Update fetch function for getting followed group's user

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,10 +5,15 @@ import "slick-carousel/slick/slick-theme.css";
 import '../Pages/Style/WelcomePage.css';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import { getToken } from '../helpers';
+import { getToken, getUserId } from '../helpers.js';
 
 const HomePage = () => {
   const [groups, setGroups] = useState([]);
+  const [userData, setUserData] = useState(null);
+  const [groupFollow, setGroupFollow] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingBanners, setLoadingBanners] = useState(true);
+
 
   useEffect(() => {
     fetch('http://localhost:1337/api/groupes?populate=*', {
@@ -62,6 +67,65 @@ const HomePage = () => {
     ]
   };
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+        try {
+            const response = await fetch(`http://localhost:1337/api/privates/${getUserId()}?populate=*`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${getToken()}`,
+                },
+            });
+    
+            if (response.ok) {
+                const data = await response.json();
+                setUserData(data.data.attributes.groupeFollow.data);
+    
+                if (data && data.data && data.data.attributes && data.data.attributes.groupeFollow.data) {
+                    const banners = await Promise.all(data.data.attributes.groupeFollow.data.map(async (groupe) => {
+                        try {
+                            const groupeResponse = await fetch(`http://localhost:1337/api/groupes/${groupe.id}?populate=*`, {
+                                method: 'GET',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    Authorization: `Bearer ${getToken()}`,
+                                },
+                            });
+    
+                            if (groupeResponse.ok) {
+                                const groupeData = await groupeResponse.json();
+                                const groupID = groupe.id
+                                console.log("test : ", groupID)
+                                return { ...groupeData.data.attributes, groupID };
+                            } else {
+                                console.error(`Erreur lors de la récupération du favori ${groupe.id}`);
+                                // Ajouter une URL de bannière par défaut ici
+                                return { data: { attributes: { url: '/default-banner.jpg' } } };
+                            }
+                        } catch (error) {
+                            console.error(error);
+                            throw new Error('Erreur lors de la récupération des bannières');
+                        }
+                    }));
+    
+                    setGroupFollow(banners);
+                }
+            } else {
+                throw new Error('Erreur lors de la récupération des données de l\'utilisateur');
+            }
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données de l\'utilisateur', error);
+            setError('Erreur lors de la récupération des données de l\'utilisateur');
+        } finally {
+            setLoading(false);
+            setLoadingBanners(false);
+        }
+    };
+
+    fetchUserData();
+}, []);
+
 return (
     <>
       <header className="bg-white flex justify-between items-center p-4">
@@ -86,45 +150,58 @@ return (
         </div>
       </header>
 
-      <section className="container mx-auto px-4 my-8">
+<section className="container mx-auto px-4 my-8">
         <h2 className="text-2xl font-bold mb-4 text-custom-blue">LES PLUS POPULAIRES</h2>
         <Slider {...settings}>
-        {groups.map((group) => (
-          <Link to={`/group/${group.id}`} key={group.id}>
-          <div key={group.id} className="group card relative p-5 transition-transform duration-300 hover:scale-105">
-            <img 
-              src={group.ImageUrl} 
-              alt={group.Titre} 
-              className="w-full h-40 object-cover rounded-lg transition duration-300 ease-in-out group-hover:opacity-50" 
-            />
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
-              <span className="text-custom-blue text-xl font-bold">{group.Titre}</span>
-            </div>
-          </div>
-          </Link>
-        ))}
+          {groups.map((group) => (
+            <Link to={`/group/${group.id}`} key={group.id}>
+              <div key={group.id} className="group card relative p-5 transition-transform duration-300 hover:scale-105">
+                <img 
+                  src={group.ImageUrl} 
+                  alt={group.Titre} 
+                  className="w-full h-40 object-cover rounded-lg transition duration-300 ease-in-out group-hover:opacity-50" 
+                />
+                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                  <span className="text-custom-blue text-xl font-bold">{group.Titre}</span>
+                </div>
+              </div>
+            </Link>
+          ))}
         </Slider>
       </section>
+
       <section className="container mx-auto px-4 my-8">
-      <h2 className="text-2xl font-bold mb-4">VOS GROUPES</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {groups.map((group) => (
-          <div key={group.id} className="group card relative mx-4 transition-transform duration-300 hover:scale-105">
-            <img 
-              src={group.ImageUrl} 
-              alt={group.Titre} 
-              className="w-full h-40 object-cover rounded-lg transition duration-300 ease-in-out group-hover:opacity-50"
-            />
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
-              <span className="text-custom-blue text-xl font-bold">{group.Titre}</span>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
-    <Navbar />
-  </>
-);
+        <h2 className="text-2xl font-bold mb-4 text-custom-blue">VOS GROUPES</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {groupFollow.map((follow) => (
+            <Link to={`/group/${follow.groupID}`} key={follow.groupID}>
+              <div className="group card relative p-5 transition-transform duration-300 hover:scale-105">
+                {follow.image.data && follow.image.data.attributes.url ? (
+                  <img
+                    className="w-full h-40 object-cover rounded-lg transition duration-300 ease-in-out group-hover:opacity-50" 
+                    src={"http://localhost:1337" + follow.image.data.attributes.url}
+                    alt={`Bannière du groupe ${follow.Titre || 'Sans titre'}`}
+                  />
+                ) : (
+                  <div className="w-full h-40 bg-gray-200 flex items-center justify-center">Aucune bannière</div>
+                )}
+                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                  {follow ? (
+                    <>
+                      <h3 className="text-lg text-center font-bold mb-2">{follow.Titre}</h3>
+                    </>
+                  ) : (
+                    <p className='text-sm text-gray-600'>Aucune information sur le groupe</p>
+                  )}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+      <Navbar />
+    </>
+  );
 };
 
 export default HomePage;
